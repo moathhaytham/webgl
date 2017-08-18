a1e84353a756fd5757ec67844a9eca18de403432
moath haytham <moath-moh@hotmail.com>
2017-08-19 00:41:48 +0200

    camera

--- webgltest/js/app.js
+++ webgltest/js/app.js
@@ -8,4 +8,16 @@ Bjsapp.init=function () {
     var engine =new BABYLON.Engine(canvas,true);
 
     var scene =new BABYLON.Scene(engine);
+
+    var camera = new BABYLON.FreeCamera('FreeCamera',new BABYLON.Vector3(0,2,-15),scene);
+
+    var light = new BABYLON.HemisphericLight('light1',new BABYLON.Vector3(0,1,0),scene);
+
+    var sphere = new BABYLON.Mesh.CreateSphere('sphere1',16,2,scene);
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    })
+
+
 };
