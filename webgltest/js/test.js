f69e5011ea58219a9bc30ed79e81bdcae657fa15
moath haytham <moath-moh@hotmail.com>
2017-08-21 11:00:21 +0200

    testupdate

--- webgltest/js/test.js
+++ webgltest/js/test.js
@@ -9,12 +9,13 @@ Bjsapp.init=function(){
 
     var scene = new BABYLON.Scene(engine);
 
-    var camera = new BABYLON.FreeCamera('c1', new BABYLON.Vector3(0,0,1),scene);
+    var camera = new BABYLON.FreeCamera('c1', new BABYLON.Vector3(0,2,-5),scene);
+    camera.attachControl(canvas);
 
 
     var light = new BABYLON.HemisphericLight('h1', new BABYLON.Vector3(0,1,0),scene);
 
-    var box = new BABYLON.Mesh.CreateSphere('s',16,2,scene);
+    var sphere = new BABYLON.Mesh.CreateSphere('s',16,2,scene);
 
 
     engine.runRenderLoop(function () {
