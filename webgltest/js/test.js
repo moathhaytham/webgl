114afac0e034b6ef41d001b2662a8b1431d997f1
moath haytham <moath-moh@hotmail.com>
2017-08-21 11:17:35 +0200

    materials and textures

--- webgltest/js/test.js
+++ webgltest/js/test.js
@@ -12,10 +12,20 @@ Bjsapp.init=function(){
     var camera = new BABYLON.FreeCamera('c1', new BABYLON.Vector3(0,2,-5),scene);
     camera.attachControl(canvas);
 
+    var ground = new BABYLON.Mesh.CreateGround('g1',100,100,2,scene);
+
+    var grass = new BABYLON.StandardMaterial('grass',scene);
+    grass.diffuseTexture= new BABYLON.Texture('assets/grass.jpg',scene);
+    ground.material=grass;
 
     var light = new BABYLON.HemisphericLight('h1', new BABYLON.Vector3(0,1,0),scene);
 
     var sphere = new BABYLON.Mesh.CreateSphere('s',16,2,scene);
+
+    var sphereMaterial= new BABYLON.StandardMaterial('mat1',scene);
+    sphereMaterial.diffuseColor= new BABYLON.Color3(0,1,0);
+    sphere.material=sphereMaterial;
+
 
 
     engine.runRenderLoop(function () {
