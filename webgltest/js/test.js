88601530f02c2c39b24f945d4e85c1c44a170ddc
moathaytham <moathaytham@localhost.localdomain>
2017-08-22 14:04:49 -0600

    new materials

--- webgltest/js/test.js
+++ webgltest/js/test.js
@@ -16,15 +16,32 @@ Bjsapp.init=function(){
 
     var grass = new BABYLON.StandardMaterial('grass',scene);
     grass.diffuseTexture= new BABYLON.Texture("Assets/grass.png",scene);
+    grass.diffuseTexture.uScale=10;
+    grass.diffuseTexture.vScale=10;
+    grass.specularColor= new BABYLON.Color3(0,0,0);
     ground.material=grass;
 
-    var light = new BABYLON.HemisphericLight('h1', new BABYLON.Vector3(0,1,0),scene);
+    //var light = new BABYLON.HemisphericLight('h1', new BABYLON.Vector3(0,1,0),scene);
 
     var sphere = new BABYLON.Mesh.CreateSphere('s',16,2,scene);
+    sphere.position=new BABYLON.Vector3(0,1,0);
+
 
     var sphereMaterial= new BABYLON.StandardMaterial('mat1',scene);
     sphereMaterial.diffuseColor= new BABYLON.Color3(0,1,0);
+    sphereMaterial.specularColor = new BABYLON.Color3(1,0,0);
+    sphereMaterial.specularPower= 1;
     sphere.material=sphereMaterial;
+
+    var box = new BABYLON.Mesh.CreateBox('b1',10,scene);
+    box.position= new BABYLON.Vector3(-7,5,0);
+
+    var boxmaterial= new BABYLON.StandardMaterial('bm',scene);
+    boxmaterial.emissiveColor=new BABYLON.Color3(1,0,0);
+    box.material=boxmaterial;
+
+
+
 
 
 
