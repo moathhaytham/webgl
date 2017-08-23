a01f28dc297f4fb10fc5a4f142f3c46d09d3d98f
moathhaytham <moath-moh@hotmail.com>
2017-08-23 09:40:42 -0600

    skybox

--- webgltest/js/app.js
+++ webgltest/js/app.js
@@ -11,13 +11,22 @@ Bjsapp.init=function () {
     scene.clearColor= new BABYLON.Color3(0,0,0);
 
     var light = new BABYLON.HemisphericLight('light',new BABYLON.Vector3(0,1,0),scene);
-    light.intensity=0.5
+    light.intensity=0.5;
     light.groundColor= new BABYLON.Color3(0,0,0);
 
     var camera = new BABYLON.ArcRotateCamera('cam1',0,0,15,new BABYLON.Vector3.Zero(),scene);
     camera.attachControl(canvas);
 
     var sun = new BABYLON.Mesh.CreateSphere('s1',16,4,scene);
+
+    var sunmaterial = new BABYLON.StandardMaterial('sunmat',scene);
+    sunmaterial.emissiveTexture= new BABYLON.Texture('Assets/sun.jpg',scene);
+    sunmaterial.diffuseColor = new BABYLON.Color3(0,0,0);
+    sunmaterial.specularColor= new BABYLON.Color3(0,0,0);
+    sun.material=sunmaterial;
+
+    var light= new BABYLON.PointLight('sunlight', new BABYLON.Vector3.Zero(),scene);
+
 
     var planet1= BABYLON.Mesh.CreateSphere('planet1',16,1,scene);
     planet1.position.x=4;
